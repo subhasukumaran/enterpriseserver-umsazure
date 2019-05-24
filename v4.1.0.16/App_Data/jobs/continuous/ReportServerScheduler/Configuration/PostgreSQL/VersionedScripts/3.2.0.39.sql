78b08440758d91b43c8223b368d9678106d20627
subhasukumaran <subha.sukumaran@syncfusion.com>
2019-05-24 07:03:46 +0530

    umslatest

--- v4.1.0.16/App_Data/jobs/continuous/ReportServerScheduler/Configuration/PostgreSQL/VersionedScripts/3.2.0.39.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE SyncRS_ScheduleDetail ADD IsSaveAsFile smallint NOT NULL default 0
-;
-ALTER TABLE SyncRS_ScheduleDetail ADD IsSendAsMail smallint NOT NULL default 0
-;
-ALTER TABLE SyncRS_ScheduleDetail ADD ReportCount int NOT NULL DEFAULT 0
-;
-ALTER TABLE SyncRS_ScheduleDetail ADD ExportPath varchar(4000) NULL
-;
--- v4.1.0.16/Configuration/PostgreSQL/VersionedScripts/3.2.0.42.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE SyncRS_ScheduleDetail ALTER COLUMN IsSendAsMail DROP default
-;
-ALTER TABLE SyncRS_ScheduleDetail ALTER COLUMN IsSendAsMail SET default 1
-;
